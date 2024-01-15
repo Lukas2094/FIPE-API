e35091e510c342baec3f82f548ae13420e9605b5
lucassilvaativo <lucas.silva@ativo.com>
2024-01-15 12:37:31 -0300

    remove error build

--- redux/reducers/carReducer.ts
+++ redux/reducers/carReducer.ts
@@ -3,27 +3,27 @@ import { createReducer } from '@reduxjs/toolkit';
 import { setCarData } from '../actions/carActions';
 
 export interface FipeData {
-  marca: string;
-  modelo: string;
-  ano: string;
-
+  brand: string;
+  models: string;
+  years: string;
 }
 
 export interface CarState {
   data: FipeData | null;
-
 }
 
 const initialState: CarState = {
   data: null,
-
 };
 
 const carReducer = createReducer(initialState, (builder) => {
   builder.addCase(setCarData, (state, action) => {
-    state.data = action.payload;
+    if (state.data) {
+      state.data.brand = action.payload.brands;
+      state.data.models = action.payload.models;
+      state.data.years = action.payload.years;
+    }
   });
-
 });
 
 export default carReducer;
